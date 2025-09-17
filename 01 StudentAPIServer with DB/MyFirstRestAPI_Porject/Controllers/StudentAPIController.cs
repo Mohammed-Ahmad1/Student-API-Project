fa0f370bfd025fa70e113b5e56498305a5de799b
Mohammed Ahmad <mfatterawi@gmail.com>
2025-09-17 14:45:00 +0300

    Update StudentAPIController.cs

--- 01 StudentAPIServer with DB/MyFirstRestAPI_Porject/Controllers/StudentAPIController.cs
+++ 01 StudentAPIServer with DB/MyFirstRestAPI_Porject/Controllers/StudentAPIController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc; 
-//using StudentApi.Models;
-//using StudentApi.DataSimulation;
 using System.Collections.Generic;
 using StudentAPIBusinessLayer;
 using StudentDataAccessLayer;
@@ -13,8 +11,6 @@ namespace StudentApi.Controllers
 
     public class StudentsController : ControllerBase // Declare the controller class inheriting from ControllerBase.
     {
-
-
         [HttpGet("All", Name ="GetAllStudents")] // Marks this method to respond to HTTP GET requests.
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -22,19 +18,13 @@ namespace StudentApi.Controllers
         //here we used StudentDTO
         public ActionResult<IEnumerable<StudentDTO>> GetAllStudents() // Define a method to get all students.
         {
-            //if (StudentDataSimulation.StudentsList.Count == 0) 
-            //{
-            //    return NotFound("No Students Found!");
-            //}
-            //return Ok(StudentDataSimulation.StudentsList); // Returns the list of students.
-
             List<StudentDTO> StudentsList = StudentAPIBusinessLayer.Student.GetAllStudents();
             if (StudentsList.Count == 0)
             {
                 return NotFound("No Students Found!");
             }
-            return Ok(StudentsList); // Returns the list of students.
 
+            return Ok(StudentsList); // Returns the list of students.
         }
 
         [HttpGet("Passed",Name = "GetPassedStudents")]
@@ -43,10 +33,7 @@ namespace StudentApi.Controllers
 
         // Method to get all students who passed
         public ActionResult<IEnumerable<StudentDTO>> GetPassedStudents()
-
         {
-            // var passedStudents = StudentDataSimulation.StudentsList.Where(student => student.Grade >= 50).ToList();
-
             List<StudentDTO> PassedStudentsList = StudentAPIBusinessLayer.Student.GetPassedStudents();
             if (PassedStudentsList.Count == 0)
             {
@@ -57,39 +44,28 @@ namespace StudentApi.Controllers
         }
 
         [HttpGet("AverageGrade", Name = "GetAverageGrade")]
-
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
         public ActionResult<double> GetAverageGrade()
         {
-            //var averageGrade = StudentDataSimulation.StudentsList.Average(student => student.Grade);
             double  averageGrade = StudentAPIBusinessLayer.Student.GetAverageGrade();
             return Ok(averageGrade);
         }
 
-
         [HttpGet("{id}", Name = "GetStudentById")]
-        
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
         public ActionResult<StudentDTO> GetStudentById(int id)
         {
-
             if (id < 1)
             {
                 return BadRequest($"Not accepted ID {id}");
             }
-
-            //var student = StudentDataSimulation.StudentsList.FirstOrDefault(s => s.Id == id);
-            //if (student == null)
-            //{
-            //    return NotFound($"Student with ID {id} not found.");
-            //}
+         
             StudentAPIBusinessLayer.Student student = StudentAPIBusinessLayer.Student.Find(id);
-
             if (student == null)
             {
                 return NotFound($"Student with ID {id} not found.");
@@ -100,7 +76,6 @@ namespace StudentApi.Controllers
 
             //we return the DTO not the student object.
             return Ok(SDTO);
-
         }
 
         //for add new we use Http Post
@@ -116,8 +91,6 @@ namespace StudentApi.Controllers
                 return BadRequest("Invalid student data.");
             }
 
-          //newStudent.Id = StudentDataSimulation.StudentsList.Count > 0 ? StudentDataSimulation.StudentsList.Max(s => s.Id) + 1 : 1;
-
           StudentAPIBusinessLayer.Student student = new StudentAPIBusinessLayer.Student( new StudentDTO(newStudentDTO.Id, newStudentDTO.Name, newStudentDTO.Age, newStudentDTO.Grade ) ) ;
             student.Save();
 
@@ -126,10 +99,7 @@ namespace StudentApi.Controllers
             //we return the DTO only not the full student object
             //we dont return Ok here,we return createdAtRoute: this will be status code 201 created.
             return CreatedAtRoute("GetStudentById", new { id = newStudentDTO.Id }, newStudentDTO);
-
         }
-
-       
 
         //here we use http put method for update
         [HttpPut("{id}", Name = "UpdateStudent")]
@@ -143,16 +113,12 @@ namespace StudentApi.Controllers
                 return BadRequest("Invalid student data.");
             }
 
-            //var student = StudentDataSimulation.StudentsList.FirstOrDefault(s => s.Id == id);
-
             StudentAPIBusinessLayer.Student student = StudentAPIBusinessLayer.Student.Find(id);
           
-
             if (student == null)
             {
                 return NotFound($"Student with ID {id} not found.");
             }
-
 
             student.Name = updatedStudent.Name;
             student.Age = updatedStudent.Age;
@@ -161,9 +127,7 @@ namespace StudentApi.Controllers
 
             //we return the DTO not the full student object.
             return Ok(student.SDTO);
-
         }
-
 
         //here we use HttpDelete method
         [HttpDelete("{id}", Name = "DeleteStudent")]
@@ -177,15 +141,11 @@ namespace StudentApi.Controllers
                 return BadRequest($"Not accepted ID {id}");
             }
 
-            // var student = StudentDataSimulation.StudentsList.FirstOrDefault(s => s.Id == id);
-            // StudentDataSimulation.StudentsList.Remove(student);
-
            if(  StudentAPIBusinessLayer.Student.DeleteStudent(id))
             
                 return Ok($"Student with ID {id} has been deleted.");
             else
                 return NotFound($"Student with ID {id} not found. no rows deleted!");
         }
-
     }
 }
